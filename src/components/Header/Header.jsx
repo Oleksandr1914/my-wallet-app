3a8a6c269b69807d8990718e3c3af9809c2e5dff
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-07-27 13:49:37 +0300

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,95 +8,41 @@ import {
   TextModal,
   ButtonModal,
 } from './Header.styled';
-import { ethers, formatEther } from 'ethers';
+import { ethers, formatEther, parseEther } from 'ethers';
 import { useEthersContext } from '../../hook/useEthersContext';
 import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 
+import React from 'react';
+
 const Header = () => {
-  const [data, setData] = useState([]);
-  const [balance, setBalance] = useState(null);
-  const [accounts, setAccounts] = useState(null);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const { setSigner } = useEthersContext();
-
-  const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
-
-  const showInstallMetaMaskMessage = () => {
-    setModalIsOpen(true);
+  const checkMetaMaskInstalled = () => {
+    return typeof window.ethereum !== 'undefined';
   };
 
   const connectWallet = async () => {
-    if (window.ethereum == null) {
-      console.log('MetaMask not installed');
-      showInstallMetaMaskMessage();
-      const provider = ethers.getDefaultProvider();
-    } else {
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      const accounts = await provider.send('eth_requestAccounts', []);
-      const balance = await provider.getBalance(accounts[0]);
-      setAccounts(accounts);
-      setBalance(formatEther(balance));
-      console.log({ accounts, balance });
-      const signer = await provider.getSigner();
-      setSigner(signer);
-      setData(accounts);
+    if (!checkMetaMaskInstalled()) {
+      alert(
+        'MetaMask not installed on your device. Please install MetaMask to connect your wallet.'
+      );
+      return;
+    }
+
+    try {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      alert('Successfully connected to MetaMask!');
+      console.log('Connected Wallet Address:', accounts[0]);
+    } catch (error) {
+      console.log('Error connecting to MetaMask:', error.message);
     }
   };
 
-  useEffect(() => {
-    if (isMobileDevice && window.ethereum == null) {
-      showInstallMetaMaskMessage();
-    }
-  }, []);
-
   return (
-    <HeaderContainer>
-      <LogoImg src={Logo} alt="logo" />
-      <ConnectBlock>
-        {data.length === 0 ? (
-          <button onClick={connectWallet}>Connect Wallet</button>
-        ) : (
-          <>
-            <TextContent>{balance && balance}</TextContent>
-            <TextContent>{accounts && accounts}</TextContent>
-          </>
-        )}
-      </ConnectBlock>
-      {/* <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        contentLabel="Install MetaMask Modal"
-      >
-        <ModalContainer>
-          <TextModal>
-            Please install MetaMask from the App Store (iOS) or Google Play
-            Store (Android) to connect your wallet.
-          </TextModal>
-          <TextModal>
-            <a
-              href="https://apps.apple.com/ua/developer/metamask/id1438144201"
-              target="_blank"
-              rel="noopener noreferrer"
-              lang="en"
-            >
-              App Store (iOS)
-            </a>
-          </TextModal>
-          <TextModal>
-            <a
-              href="https://play.google.com/store/apps/details?id=io.metamask&hl=ru&gl=US"
-              target="_blank"
-              rel="noopener noreferrer"
-              lang="en"
-            >
-              Google Play Store (Android)
-            </a>
-          </TextModal>
-          <ButtonModal onClick={() => setModalIsOpen(false)}>Close</ButtonModal>
-        </ModalContainer>
-      </Modal> */}
-    </HeaderContainer>
+    <div>
+      <button onClick={connectWallet}>Connect Wallet</button>
+    </div>
   );
 };
 
